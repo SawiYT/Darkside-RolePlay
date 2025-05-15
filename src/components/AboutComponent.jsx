861b6ef06a2a8269b4286dbb3fcf65be1b0319b9
Patryk <patrykzielinski123321@gmail.com>
2025-05-15 20:33:27 +0200

    about section improved

--- src/components/AboutComponent.jsx
+++ src/components/AboutComponent.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { IconDiamond } from '@tabler/icons-react';
 
 const ShopComponent = ({ icon, title, description }) => {
 	return (
-		<div className='relative border-2 border-black rounded-lg cursor-pointer w-80 overflow-hidden shadow-[5px_5px_0px_1px_black] transition-transform duration-100 ease-in-out '>
+		<div className='flex justify-center relative  border-2 border-black rounded-lg cursor-pointer w-[22rem] overflow-hidden shadow-[5px_5px_0px_1px_black] transition-transform duration-100 ease-in-out '>
+			<p className='text-[#f92763] pt-5 text-2xl'>
+				{icon}
+			</p>
 			<div className='flex flex-col items-start to-transparent px-4 pt-4'>
 				<div className='text-2xl flex items-center gap-4 font-raleway font-bold text-white shadow-md mb-2'>{title}</div>
 				<p className='opacity-65 text-white text-md shadow-sm shadow-black pr-4'>{description}</p>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,48 +4,48 @@ const Footer = () => {
 	const currentYear = new Date().getFullYear();
 
 	return (
-		<div className='flex justify-center items-center text-white bg-[#0e0d0d] h-auto lg:h-60'>
-			<div className='flex flex-col lg:flex-row justify-between items-center gap-8 lg:gap-20'>
-				<div className='flex justify-center items-center flex-col'>
-					<img className='h-32' src='/logo2.png' alt='Logo' />
-					<h2 className='text-white text-lg font-bold uppercase  lg:text-xl text-center mt-2'>
+		<div className='bg-[#0e0d0d] text-white py-20 px-6 lg:px-20'>
+			<div className='max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-12'>
+				{/* Left Section: Logo and Copyright */}
+				<div className='flex flex-col items-center lg:items-start'>
+					<img className='h-32 mb-4' src='/logo2.png' alt='Logo' />
+					<h2 className='text-2xl font-bold uppercase text-center lg:text-left'>
 						COPYRIGHT {currentYear} AmazingDev.PL
 					</h2>
+					<p className='mt-4 text-sm opacity-80 text-center lg:text-left max-w-md'>
+						This project is not affiliated with or endorsed by Rockstar North, Take-Two Interactive, or any other
+						rightsholders. All trademarks belong to their respective owners.
+					</p>
 				</div>
+
+				{/* Right Section: Contact and Links */}
 				<div className='flex flex-col items-center lg:items-start'>
-					<p className='text-xl lg:text-3xl underline pb- lg:pb-4 font-bold uppercase font-raleway decoration-[#f92763] underline-offset-[5px]'>
-						Useful Links
+					<p className='text-2xl font-bold uppercase underline decoration-[#f92763] underline-offset-4 mb-4'>
+						Contact & Info
+					</p>
+					<ul className='space-y-2 text-lg'>
+						<li>
+							<span className='font-semibold'>Email:</span>{' '}
+							<a href='mailto:contact@darkside.pl' className='text-[#f92763] hover:underline'>
+								contact@darkside.pl
+							</a>
+						</li>
+						<li>
+							<span className='font-semibold'>Forum:</span>{' '}
+							<a href='https://forum-darkside.pl' className='text-[#f92763] hover:underline'>
+								forum-darkside.pl
+							</a>
+						</li>
+					</ul>
+
+					<p className='mt-10 text-sm opacity-80 max-w-md text-center lg:text-left'>
+						This server is built by the community, for the community. We are independent developers and do not represent
+						any official game publisher or platform.
 					</p>
 
-					<div className='flex flex-col lg:flex-row gap-2 font-raleway lg:gap-3 pb-2'>
-						<a className='border-l-4 border-[#f92763] font-bold uppercase  pl-2 lg:pl-3' href=''>
-							Home
-						</a>
-						<a className='border-l-4 border-[#f92763] font-bold uppercase  pl-2 lg:pl-3' href=''>
-							Terms of Service
-						</a>
-						<a className='border-l-4 border-[#f92763] font-bold uppercase pl-2 lg:pl-3' href=''>
-							Privacy Policy
-						</a>
-						<a className='border-l-4 border-[#f92763] font-bold uppercase  pl-2 lg:pl-3' href=''>
-							FAQ
-						</a>
-					</div>
-
-					<div className='text-center pt-8 lg:pt-0 lg:text-left'>
-						<p>
-							PL - Serwer amazingdev.pl nie jest w żaden sposób powiązany z firmą Rockstar Games, Take-Two Interactive
-							ani z platformą FiveM.
-						</p>
-						<p>
-							EN - The amazingdev.pl server is not associated with Rockstar Games, Take-Two Interactive, or the FiveM
-							platform in any way.
-						</p>
-
-						<p className='pt-2 lg:pt-4 font-bold uppercase font-raleway'>
-							Created by <span className='text-[#f92763]'>ValeryR</span>
-						</p>
-					</div>
+					<p className='mt-6 text-md font-bold uppercase font-raleway'>
+						Created by <span className='text-[#f92763]'>ValeryR</span>
+					</p>
 				</div>
 			</div>
 		</div>
